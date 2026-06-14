6ce360a5500a040499dcfe383a49fc26b053da95
Zwf5458-Py <bitonekf@gmail.com>
2026-06-14 11:17:33 +0800

    chore: support PORT env var in gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,8 @@ import os
 
 # Gunicorn 配置文件
 
-bind = f"{os.environ.get('FLASK_HOST', '0.0.0.0')}:{os.environ.get('FLASK_PORT', '5000')}"
+port = os.environ.get('PORT', os.environ.get('FLASK_PORT', '5000'))
+bind = f"{os.environ.get('FLASK_HOST', '0.0.0.0')}:{port}"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gthread"
 threads = 4
